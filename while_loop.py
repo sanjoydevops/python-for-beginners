77b033b440b4833ffc6ade4d1557b68506be6df4
sanjoydeb0007 <sanjoydeb0007@gmail.com>
2021-07-19 16:04:51 +0600

    Different types of while loop

---
+++ while_loop.py
@@ -0,0 +1,31 @@
+#While loop
+i=1
+while i<6:
+    print(i)
+    i=i+1
+#while loop break
+a=1
+while a<=6:
+    print(a)
+    if a ==3:
+        break
+    a=a+1
+#while loop continue
+b=1
+while b<=4:
+    b=b+1
+    if b==3:
+        continue
+    print(b)
+#while loop else statement
+c=1
+while c<=6:
+    print(c)
+    c=c+1
+else:
+    print("i is no longer less than 6")
+#exercise Print i as long as i is less than 6.
+x=1
+while x<6:
+    print(x)
+    x=x+1
